d8e8ce93a183067240ac613930c7ffb3619111e0
kkirru <qorgusdn04@gmail.com>
2020-02-06 01:23:27 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// // children component
+// function Food({ name, picture, rating }) {
+//   return <div>
+//     <h2> I love {name}</h2>
+//     <h4> ratings : {rating}/5.0</h4>
+//     <img src={picture} />
+//   </div>
+// }
+
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number // number 혹은 unidentified
+// }
+
+// // function component
+// // 무언가 리턴함
+// function App() {
+//   return (
+//     // html
+//     <div className="App"> <h1>
+//       {/* dish : object */} 
+//       {/* map : array item  돌며 실행, return array 만듬 */}
+//       {foodILike.map(dish => <Food key={dish.id}
+//         name={dish.name}
+//         picture={dish.image}
+//         rating={dish.rating} />)}
+
+//     </h1>
+//     </div>
+//   );
+// }
+
+// class Component > state 사용하기 위함.
+class App extends React.Component {
+  // state 는 object, 변하는 data를 담음
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    // no! render function을 refresh하지 않음. > setstate 사용
+    // this.state.count ++;
+
+    // *** setstate => state refresh 하고 render 호출 ***
+    // this.setState({count: this.state.count + 1}); // 좋은 방법 X (성능문제)
+
+    this.setState(current => ({count : current.count + 1}));
+  }
+
+  minus = () => {
+    this.setState(current => ({count : current.count - 1}));
+
+  }
+ 
+  // render 메서드는 자동 실행
+  render() {
+    return (
+      <div>
+        <h1> the number is :  {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));  //<App /> : Component (HTML을 반환하는 함수
+// public/index.html의 root id를 찾아 모든 요소를 넣어줌
+// 리액트는 소스코드(index.html)에 바로 html을 쓰지 않고 html을 삽입하거나 제거함
+// 시작 시 빈 html를 로드하고 html을 밀어넣음
+// > virtual! react가 빠른 이유
